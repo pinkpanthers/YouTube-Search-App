8c85428d3c73502d14ee1c1eb2cf5d6ea71f8104
Sterling Ortiz <sortizp@bu.edu>
2019-06-15 14:10:59 -0400

    Ready to add YouTube Data API (v3)

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Search bar handler
+$(function() {
+  var searchField = $('#query');
+  var icon = $('#search-btn');
+
+  // Focus event handler
+  $(searchField).on('focus', function() {
+    $(this).animate({
+      width: '100%' }, 400);
+    $(icon).animate({
+      right: '10px' }, 400);
+  });
+
+  // Blur event handler
+  $(searchField).on('blur', function() {
+    if (searchField.val() == '') {
+        $(searchField).animate({
+          width: '45%' }, 400, function(){});
+        $(icon).animate({
+          right: '360px' }, 400, function(){});
+    }
+  });
+
+})
